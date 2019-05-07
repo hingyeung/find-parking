7fcdba321667882e3f93f672888c9d1fba3abbca
Samuel Li <samli@samuelli.net>
2019-05-08 00:16:18 +1000

    added API gateway usage plan. updated cloudwatch event schedule with GMT time.

--- scripts/deployment/package.sh
+++ scripts/deployment/package.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 source `dirname $0`/../commons.sh
+OUTPUT_CF_DIR=${PROJECT_ROOT}/tmp
+mkdir -p ${OUTPUT_CF_DIR}
 
 SAM_TEMPLATE=${PROJECT_ROOT}/deploy/template.yaml
-OUTPUT_CF_TEMPLATE=${PROJECT_ROOT}/tmp/find_parking_cfn.yaml
+OUTPUT_CF_TEMPLATE=${OUTPUT_CF_DIR}/find_parking_cfn.yaml
 ARTEFACT_S3_BUCKET=$1
 ARTEFACT_S3_PREFIX=$2
 
